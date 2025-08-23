cfad1dec10fafeb4ab6747aca8f34a02c35aadcd
Vandan Thakkar <109749192+Vandan1207@users.noreply.github.com>
2025-08-23 23:20:36 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        // BIT MANIPULATTION
-        long N = n;
-         if(N < 0){
-            N = -N;
-            x = 1 / x;
-        }
-        
-        double ans =1;
-        while(N>0){
-            if((N&1)!=0){
-                ans*=x;
+          long N = n;
+            if(n<0){
+                 N=N*-1;
+                x=1/x;
             }
-            x*=x;
-            N=N>>1;
-        }
-        return ans;
+            double ans = 1;
+            while(N!=0){
+                if((N&1)==1){
+                    ans = ans *x;
+                }
+                x*=x;
+                N=N>>1;
+            }
+            
+
+            return ans;
     }
 }
