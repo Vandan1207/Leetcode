6f46bd851c0e2b07f1bfc618db567a9f6f3ec7cf
Vandan Thakkar <109749192+Vandan1207@users.noreply.github.com>
2026-03-26 17:47:00 +0530

    Time: 273 ms (42.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-SELECT IFNULL((SELECT DISTINCT salary  FROM Employee AS e1 WHERE 2 = (
-    SELECT COUNT(DISTINCT salary) FROM Employee AS e2 WHERE e1.salary<=e2.salary
-)),NULL) AS SecondHighestSalary
+-- SELECT IFNULL((SELECT DISTINCT salary  FROM Employee AS e1 WHERE 2 = (
+--     SELECT COUNT(DISTINCT salary) FROM Employee AS e2 WHERE e1.salary<=e2.salary
+-- )),NULL) AS SecondHighestSalary
+
+SELECT (SELECT DISTINCT salary  FROM  Employee ORDER BY salary DESC  LIMIT 1 OFFSET 1) AS SecondHighestSalary
