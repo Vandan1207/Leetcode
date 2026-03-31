5325f8ff052921c295993898f7cb984f177eb431
Vandan Thakkar <109749192+Vandan1207@users.noreply.github.com>
2026-03-31 22:26:38 +0530

    Time: 106 ms (30%), Space: 77.9 MB (65.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,44 +1,46 @@
 class Solution {
-
-
-    
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int prevSmallest[] = new int[n];
         Stack<Integer> s1 = new Stack<>();
-        for(int i =0;i<n;i++){
-            while(!s1.isEmpty() && heights[s1.peek()]>= heights[i]){
+        Stack<Integer> s2 = new Stack<>();
+        int n = heights.length;
+        int leftMax[]= new int[n];
+        int rightMax[]= new int[n];
+
+        for(int i=0;i<n;i++){
+            int curr = heights[i];
+            while(!s1.isEmpty() && heights[s1.peek()]>=curr){
                 s1.pop();
             }
             if(s1.isEmpty()){
-                prevSmallest[i] = -1;
+                leftMax[i]= -1;
             }else{
-                prevSmallest[i] = s1.peek();
+                leftMax[i] = s1.peek();
             }
-                s1.push(i);
+            s1.push(i);
         }
 
-        int nextSmallest[] = new int[n];
-        Stack<Integer> s2 = new Stack<>();
         for(int i=n-1;i>=0;i--){
-              while(!s2.isEmpty() && heights[s2.peek()]>= heights[i]){
+            int curr = heights[i];
+            while(!s2.isEmpty() && heights[s2.peek()]>=curr){
                 s2.pop();
             }
+
             if(s2.isEmpty()){
-                nextSmallest[i] = n;
+                rightMax[i]=n;
             }else{
-                nextSmallest[i] = s2.peek();
+                rightMax[i] = s2.peek();
             }
-                s2.push(i);
-        }
-        int maxArea = 0;
-        for(int i =0;i<n;i++){
-            int length =heights[i];
-            int breath = nextSmallest[i] - prevSmallest[i] -1 ;
-            int area = length * breath;
-            maxArea = Math.max(maxArea,area);
+            s2.push(i);
         }
 
-        return maxArea;    
+        int maxArea =0;
+        for(int i=0;i<n;i++){
+            int area = (rightMax[i] - leftMax[i] -1) * heights[i] ;
+            maxArea = Math.max(area,maxArea);
+        }
+        return maxArea;
+
+
+
     }
 }
